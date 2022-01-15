551a49c595c04945d9f15b0ba7e3ed81fe79efe1
Dimitar Beshev <beshev1@gmail.com>
2022-01-15 20:53:32 +0200

    Initial Upload (with Template).

---
+++ src/FitnessBuddy.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessBuddy.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "FitnessBuddy";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ src/Services/FitnessBuddy.Services.Mapping/IMapFrom.cs
@@ -0,0 +1,7 @@
+﻿namespace FitnessBuddy.Services.Mapping
+{
+    // ReSharper disable once UnusedTypeParameter
+    public interface IMapFrom<T>
+    {
+    }
+}
