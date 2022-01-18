ac02ee8211965a969b1613d61fe46734a20e2574
Dimitar Beshev <beshev1@gmail.com>
2022-01-18 21:19:54 +0200

    Added DataConstants class.

---
+++ src/FitnessBuddy.Common/DataConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace FitnessBuddy.Common
+{
+    public class DataConstants
+    {
+        // UserFavoriteFoods table
+        public const string UserFavoriteFoodTableName = "UsersFavoriteFoods";
+        public const string FoodIdPropertyName = "FoodId";
+        public const string UserIdPropertyName = "UserId";
+    }
+}
