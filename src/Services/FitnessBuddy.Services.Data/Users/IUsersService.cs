48d39ba5e3543a6bbad3c6bfdea4f69612b54047
Dimitar Beshev <beshev1@gmail.com>
2022-01-19 16:26:51 +0200

    Added UsersService.cs file.

---
+++ src/Services/FitnessBuddy.Services.Data/Users/IUsersService.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessBuddy.Services.Data.Users
+{
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Web.ViewModels.Users;
+
+    public interface IUsersService
+    {
+        public UserProfileInputModel GetUserInfo(string userId);
+
+        public Task EditAsync(string userId, UserProfileInputModel model);
+    }
+}
---
+++ src/Services/FitnessBuddy.Services.Data/Users/UsersService.cs
@@ -0,0 +1,61 @@
+﻿namespace FitnessBuddy.Services.Data.Users
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Data.Common.Repositories;
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Web.ViewModels.Users;
+
+    public class UsersService : IUsersService
+    {
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
+
+        public UsersService(
+            IDeletableEntityRepository<ApplicationUser> usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+
+        public async Task EditAsync(string userId, UserProfileInputModel model)
+        {
+            var user = this.usersRepository
+                .All()
+                .FirstOrDefault(x => x.Id == userId);
+
+            user.UserName = model.Username;
+            user.WeightInKg = model.WeightInKg;
+            user.GoalWeightInKg = model.GoalWeightInKg;
+            user.ProfilePicture = model.ProfilePicture;
+            user.HeightInCm = model.HeightInCm;
+            user.DailyProteinGoal = model.DailyProteinGoal;
+            user.DailyCarbohydratesGoal = model.DailyCarbohydratesGoal;
+            user.DailyFatGoal = model.DailyFatGoal;
+            user.AboutMe = model.AboutMe;
+
+            await this.usersRepository.SaveChangesAsync();
+        }
+
+        public UserProfileInputModel GetUserInfo(string userId)
+        {
+            var user = this.usersRepository
+                .AllAsNoTracking()
+                .Where(x => x.Id == userId)
+                .Select(userData => new UserProfileInputModel
+                {
+                    Username = userData.UserName,
+                    WeightInKg = userData.WeightInKg,
+                    GoalWeightInKg = userData.GoalWeightInKg,
+                    ProfilePicture = userData.ProfilePicture,
+                    HeightInCm = userData.HeightInCm,
+                    DailyProteinGoal = userData.DailyProteinGoal,
+                    DailyCarbohydratesGoal = userData.DailyCarbohydratesGoal,
+                    DailyFatGoal = userData.DailyFatGoal,
+                    AboutMe = userData.AboutMe,
+                })
+                .FirstOrDefault();
+
+            return user;
+        }
+    }
+}
