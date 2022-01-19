a09e4d23fcf4af260bd6dc3361c1e946d42632aa
Dimitar Beshev <beshev1@gmail.com>
2022-01-19 16:26:22 +0200

    Added DataConstants class.

--- src/FitnessBuddy.Common/DataConstants.cs
+++ src/FitnessBuddy.Common/DataConstants.cs
@@ -6,5 +6,36 @@
         public const string UserFavoriteFoodTableName = "UsersFavoriteFoods";
         public const string FoodIdPropertyName = "FoodId";
         public const string UserIdPropertyName = "UserId";
+
+        // ApplicationUser model
+        public const int UserUsernameMinLength = 4;
+        public const int UserUsernameMaxLength = 25;
+
+        public const double UserWeightMinValue = 1;
+        public const double UserWeightMaxValue = 300;
+
+        public const double UserHeightMinValue = 0;
+        public const double UserHeightMaxValue = 300;
+
+        public const double UserDailyNutritionsMinValue = 0;
+        public const double UserDailyNutritionsMaxValue = 1000;
+
+        public const int UserAboutMeMinLength = 10;
+        public const int UserAboutMeMaxLength = 500;
+
+        // Food model
+        public const int FoodDescriptionMinLength = 10;
+        public const int FoodDescriptionMaxLength = 500;
+
+        public const double FoodNutritionsMinValue = 0;
+        public const double FoodNutritionsMaxValue = 1000;
+
+        // FoodName model
+        public const int FoodNameMinLength = 1;
+        public const int FoodNameMaxLength = 50;
+
+        // Meal model
+        public const int MealNameMinLength = 1;
+        public const int MealNameMaxLength = 50;
     }
 }
