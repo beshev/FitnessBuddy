2928dfd897704349c5b9a3da27e16f5544730730
Dimitar Beshev <beshev1@gmail.com>
2022-01-25 18:38:40 +0200

    Change delete methods from "SoftDelete" to "HardDelete".

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Meals/MealDropDownViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Meals
+{
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class MealDropDownViewModel : IMapFrom<Meal>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web/ViewComponents/UserMealsDropDownViewComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace FitnessBuddy.Web.ViewComponents
+{
+    using FitnessBuddy.Services.Data.Meals;
+    using FitnessBuddy.Web.ViewModels.Meals;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserMealsDropDownViewComponent : ViewComponent
+    {
+        private readonly IMealsService mealsService;
+
+        public UserMealsDropDownViewComponent(IMealsService mealsService)
+        {
+            this.mealsService = mealsService;
+        }
+
+        public IViewComponentResult Invoke(string userId)
+        {
+            var viewModel = this.mealsService.GetUserMeals<MealDropDownViewModel>(userId);
+
+            return this.View(viewModel);
+        }
+    }
+}
