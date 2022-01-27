08b5b264c041aae2971407e873c831a6ce60ffdc
Dimitar Beshev <beshev1@gmail.com>
2022-01-27 18:37:52 +0200

    Add new property "ImageUrl". Add mapping interface.

--- src/Web/FitnessBuddy.Web.ViewModels/Meals/MealFoodInputModel.cs
+++ src/Web/FitnessBuddy.Web.ViewModels/Meals/MealFoodInputModel.cs
@@ -2,13 +2,18 @@
 {
     using System.ComponentModel.DataAnnotations;
 
+    using AutoMapper;
     using FitnessBuddy.Common;
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
 
-    public class MealFoodInputModel
+    public class MealFoodInputModel : IMapFrom<Food>, IHaveCustomMappings
     {
+        public int FoodId { get; set; }
+
         public string FoodName { get; set; }
 
-        public int FoodId { get; set; }
+        public string ImageUrl { get; set; }
 
         public int MealId { get; set; }
 
@@ -18,5 +23,12 @@
             ErrorMessage = "Quantity must be between {1} and {2}.")]
         [Display(Name = "Quantity in grams")]
         public double QuantityInGrams { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Food, MealFoodInputModel>()
+                .ForMember(dest => dest.FoodName, opt => opt.MapFrom(x => x.FoodName.Name))
+                .ForMember(dest => dest.FoodId, opt => opt.MapFrom(x => x.Id));
+        }
     }
 }
