5d3feae23c8ee864fbdb944b8b2815dc4466a822
Dimitar Beshev <beshev1@gmail.com>
2022-01-31 20:20:24 +0200

    Revert "Add border."
    This reverts commit 45c56c1c07f2680e47b92b8664262c508f7e05f4.

--- src/Web/FitnessBuddy.Web.ViewModels/Exercises/ExerciseViewModel.cs
+++ src/Web/FitnessBuddy.Web.ViewModels/Exercises/ExerciseViewModel.cs
@@ -5,8 +5,6 @@
 
     public class ExerciseViewModel : IMapFrom<Exercise>
     {
-        public int Id { get; set; }
-
         public string Name { get; set; }
 
         public string Difficulty { get; set; }
