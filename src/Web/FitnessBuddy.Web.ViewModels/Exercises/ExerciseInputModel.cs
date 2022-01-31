915d7fdb998cdcd0346fff9a0b6e9c20cac76d25
Dimitar Beshev <beshev1@gmail.com>
2022-01-31 17:06:27 +0200

    Add two models "ExerciseInputModel.cs" and "ExerciseViewModel.cs".

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Exercises/ExerciseInputModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Exercises
+{
+    using FitnessBuddy.Data.Models.Enums;
+
+    public class ExerciseInputModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ExerciseDifficulty Difficulty { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string VideoUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int EquipmentId { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web.ViewModels/Exercises/ExerciseViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Exercises
+{
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class ExerciseViewModel : IMapFrom<Exercise>
+    {
+        public string Name { get; set; }
+
+        public string Difficulty { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string EquipmentName { get; set; }
+    }
+}
