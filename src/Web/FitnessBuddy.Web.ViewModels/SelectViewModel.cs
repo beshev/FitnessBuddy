d688dad31965c0e084e5161afde853b304b95c37
Dimitar Beshev <beshev1@gmail.com>
2022-01-31 17:09:30 +0200

    Add model "SelectViewModel.cs" for using from view components.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/SelectViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FitnessBuddy.Web.ViewModels
+{
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class SelectViewModel :
+        IMapFrom<ExerciseCategory>,
+        IMapFrom<ExerciseEquipment>,
+        IMapFrom<Meal>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
