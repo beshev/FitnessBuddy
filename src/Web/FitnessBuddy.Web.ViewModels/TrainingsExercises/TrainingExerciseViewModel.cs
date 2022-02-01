48cbc41ee0a93533a3463e4dfffd085857289f5c
Dimitar Beshev <beshev1@gmail.com>
2022-02-01 22:06:05 +0200

    Add TrainingExerciseViewModel.cs

---
+++ src/Web/FitnessBuddy.Web.ViewModels/TrainingsExercises/TrainingExerciseViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnessBuddy.Web.ViewModels.TrainingsExercises
+{
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class TrainingExerciseViewModel : IMapFrom<TrainingExercise>
+    {
+        public string ExerciseName { get; set; }
+
+        public string ExerciseImageUrl { get; set; }
+
+        public int Sets { get; set; }
+
+        public int Repetitions { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
