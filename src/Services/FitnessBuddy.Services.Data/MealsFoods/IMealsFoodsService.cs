636bf30c3c78aadbcd35e4f92013a685d7229a9f
Dimitar Beshev <beshev1@gmail.com>
2022-02-02 14:29:27 +0200

    .

---
+++ src/Services/FitnessBuddy.Services.Data/MealsFoods/IMealsFoodsService.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnessBuddy.Services.Data.MealsFoodsService
+{
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Web.ViewModels.Meals;
+
+    public interface IMealsFoodsService
+    {
+        public MealFood GetById(int mealFoodId);
+
+        public bool Contains(int mealFoodId);
+
+        public Task<MealFood> DeleteAsync(MealFood mealFood);
+
+        public Task<MealFood> AddAsync(MealFoodInputModel model);
+    }
+}
