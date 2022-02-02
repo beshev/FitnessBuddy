322190bb7181a70fc258bc99589203d9d7602ff4
Dimitar Beshev <beshev1@gmail.com>
2022-02-02 14:30:00 +0200

    Add new TrainigsExercises service.

---
+++ src/Services/FitnessBuddy.Services.Data/TrainingsExercises/ITrainingsExercisesService.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessBuddy.Services.Data.TrainingsExercises
+{
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Web.ViewModels.Exercises;
+
+    public interface ITrainingsExercisesService
+    {
+        public Task AddAsync(ExerciseTrainingInputModel model);
+    }
+}
---
+++ src/Services/FitnessBuddy.Services.Data/TrainingsExercises/TrainingsExercisesService.cs
@@ -0,0 +1,42 @@
+﻿namespace FitnessBuddy.Services.Data.TrainingsExercises
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Data.Common.Repositories;
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Web.ViewModels.Exercises;
+
+    public class TrainingsExercisesService : ITrainingsExercisesService
+    {
+        private readonly IDeletableEntityRepository<TrainingExercise> trainingExerciseRepository;
+
+        public TrainingsExercisesService(IDeletableEntityRepository<TrainingExercise> trainingExerciseRepository)
+        {
+            this.trainingExerciseRepository = trainingExerciseRepository;
+        }
+
+        public async Task AddAsync(ExerciseTrainingInputModel model)
+        {
+            var trainingExercise = this.trainingExerciseRepository
+                .All()
+                .FirstOrDefault(x => x.ExerciseId == model.Id && x.TrainingId == model.TrainingId);
+
+            if (trainingExercise == null)
+            {
+                trainingExercise = new TrainingExercise
+                {
+                    ExerciseId = model.Id,
+                    TrainingId = model.TrainingId,
+                };
+            }
+
+            trainingExercise.Sets = model.Sets;
+            trainingExercise.Repetitions = model.Repetitions;
+            trainingExercise.Weight = model.Weight;
+
+            await this.trainingExerciseRepository.AddAsync(trainingExercise);
+            await this.trainingExerciseRepository.SaveChangesAsync();
+        }
+    }
+}
