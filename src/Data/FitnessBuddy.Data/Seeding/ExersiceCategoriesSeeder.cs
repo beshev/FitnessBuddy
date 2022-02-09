8228f13a160e4efddf0cb7285e8aecb7b733481a
Dimitar Beshev <beshev1@gmail.com>
2022-02-09 17:22:38 +0200

    Add exercise equipment seeder.

---
+++ src/Data/FitnessBuddy.Data/Seeding/ExersiceCategoriesSeeder.cs
@@ -0,0 +1,81 @@
+﻿namespace FitnessBuddy.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Data.Models;
+
+    public class ExersiceCategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.ExerciseCategories.Any())
+            {
+                return;
+            }
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Shoulders",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Triceps",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Biceps",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Back",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Chest",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Forearm",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Traps",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Abs",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Glutes",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Quadriceps",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Hamstrings",
+            });
+
+            dbContext.ExerciseCategories.Add(new ExerciseCategory
+            {
+                Name = "Calves",
+            });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
