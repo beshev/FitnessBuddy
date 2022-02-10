09cc177e687f0e734eee83ec0fb7419645883a3a
Dimitar Beshev <beshev1@gmail.com>
2022-02-10 20:12:25 +0200

    Add create article view.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Articles/ArticleInputModel.cs
@@ -0,0 +1,28 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Articles
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using FitnessBuddy.Common;
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class ArticleInputModel : IMapTo<Article>
+    {
+        [Required]
+        [StringLength(DataConstants.ArticleTitleMaxLength, MinimumLength = DataConstants.ArticleTitleMinLength, ErrorMessage = "Title lenght must be betweem {2} and {1}")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(DataConstants.ArticleContentMaxLength, MinimumLength = DataConstants.ArticleContentMinLength, ErrorMessage = "Content lenght must be betweem {2} and {1}")]
+        public string Content { get; set; }
+
+        [Required]
+        [Url(ErrorMessage = "The URL is not valid!")]
+        public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "The field Category is required!")]
+        public int? CategoryId { get; set; }
+
+        public string AddedByUserId { get; set; }
+    }
+}
