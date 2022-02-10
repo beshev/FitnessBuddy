c2f8f1c2796375ab13622a8fd6cc8a7fb9569bdf
Dimitar Beshev <beshev1@gmail.com>
2022-02-10 20:11:26 +0200

    Add article categories view component.

---
+++ src/Web/FitnessBuddy.Web/ViewComponents/ArticleCategoriesSelectViewComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace FitnessBuddy.Web.ViewComponents
+{
+    using FitnessBuddy.Services.Data.Articles;
+    using FitnessBuddy.Web.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ArticleCategoriesSelectViewComponent : ViewComponent
+    {
+        private readonly IArticleCategoriesService articleCategoriesService;
+
+        public ArticleCategoriesSelectViewComponent(IArticleCategoriesService articleCategoriesService)
+        {
+            this.articleCategoriesService = articleCategoriesService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var viewModel = this.articleCategoriesService.GetAll<SelectViewModel>();
+
+            return this.View(viewModel);
+        }
+    }
+}
