0341705a01fe348aabc13917b3e153357a501172
Dimitar Beshev <beshev1@gmail.com>
2022-02-10 20:10:45 +0200

    Add articles controller.

---
+++ src/Web/FitnessBuddy.Web/Controllers/ArticlesController.cs
@@ -0,0 +1,47 @@
+﻿namespace FitnessBuddy.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Services.Data.Articles;
+    using FitnessBuddy.Web.Infrastructure.Extensions;
+    using FitnessBuddy.Web.ViewModels.Articles;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class ArticlesController : Controller
+    {
+        private readonly IArticlesService articlesService;
+
+        public ArticlesController(IArticlesService articlesService)
+        {
+            this.articlesService = articlesService;
+        }
+
+        [AllowAnonymous]
+        public IActionResult All()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ArticleInputModel model)
+        {
+            if (this.ModelState.IsValid == false)
+            {
+                return this.View(model);
+            }
+
+            model.AddedByUserId = this.User.GetUserId();
+
+            await this.articlesService.CreateAsync(model);
+
+            return this.RedirectToAction(nameof(this.All));
+        }
+    }
+}
