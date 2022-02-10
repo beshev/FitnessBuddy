6d803fe82bbbb3118fbb543dfbba89f95bf5ef4e
Dimitar Beshev <beshev1@gmail.com>
2022-02-10 13:23:36 +0200

    Add exercises seeder.

--- src/Data/FitnessBuddy.Data/Seeding/ApplicationDbContextSeeder.cs
+++ src/Data/FitnessBuddy.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -28,6 +28,7 @@
                               new RolesSeeder(),
                               new ExersiceCategoriesSeeder(),
                               new ExerciseEquipmentsSeeder(),
+                              new ExercisesSeeder(),
                           };
 
             foreach (var seeder in seeders)
