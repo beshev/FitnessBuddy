804fad89b3314189283cf969d9cb0ebf3d8c8dcb
Dimitar Beshev <beshev1@gmail.com>
2022-02-21 18:50:43 +0200

    Change the way for taking user Id.

--- src/Web/FitnessBuddy.Web/ViewComponents/TrainingsSelectViewComponent.cs
+++ src/Web/FitnessBuddy.Web/ViewComponents/TrainingsSelectViewComponent.cs
@@ -1,6 +1,7 @@
 ﻿namespace FitnessBuddy.Web.ViewComponents
 {
     using FitnessBuddy.Services.Data.Trainings;
+    using FitnessBuddy.Web.Infrastructure.Extensions;
     using FitnessBuddy.Web.ViewModels;
     using Microsoft.AspNetCore.Mvc;
 
@@ -13,9 +14,9 @@
             this.trainingsService = trainingsService;
         }
 
-        public IViewComponentResult Invoke(string userId)
+        public IViewComponentResult Invoke()
         {
-            var viewModel = this.trainingsService.GetAll<SelectViewModel>(userId);
+            var viewModel = this.trainingsService.GetAll<SelectViewModel>(this.UserClaimsPrincipal.GetUserId());
 
             return this.View(viewModel);
         }
