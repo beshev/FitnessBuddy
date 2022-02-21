c63143f93da35c004dec3618861b699c0574f572
Dimitar Beshev <beshev1@gmail.com>
2022-02-21 18:51:06 +0200

    Add new view model.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Exercises/ExerciseDetailsModel.cs
@@ -0,0 +1,27 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Exercises
+{
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+    using FitnessBuddy.Web.ViewModels.Trainings;
+
+    public class ExerciseDetailsModel : IMapFrom<Exercise>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Difficulty { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string VideoUrl { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string EquipmentName { get; set; }
+
+        public TrainingExerciseInputModel TrainingExercise { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web.ViewModels/Trainings/TrainingExerciseInputModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Trainings
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using FitnessBuddy.Common;
+
+    public class TrainingExerciseInputModel
+    {
+        public int TrainingId { get; set; }
+
+        public int ExerciseId { get; set; }
+
+        [Range(DataConstants.ExerciseSetsMinValue, DataConstants.ExerciseSetsMaxValue)]
+        public int Sets { get; set; }
+
+        [Range(DataConstants.ExerciseRepetitionMinValue, DataConstants.ExerciseRepetitionMaxValue)]
+        public int Repetitions { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
