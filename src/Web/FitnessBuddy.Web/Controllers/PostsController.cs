9a7eabd8bf978aeec5ee2d53359f59bca9d018e9
Dimitar Beshev <beshev1@gmail.com>
2022-02-23 19:12:30 +0200

    Add action for creating a post. Add action listing all posts.

--- src/Web/FitnessBuddy.Web/Controllers/PostsController.cs
+++ src/Web/FitnessBuddy.Web/Controllers/PostsController.cs
@@ -1,6 +1,9 @@
 ﻿namespace FitnessBuddy.Web.Controllers
 {
+    using System.Threading.Tasks;
+
     using FitnessBuddy.Services.Data.Posts;
+    using FitnessBuddy.Web.Infrastructure.Extensions;
     using FitnessBuddy.Web.ViewModels.Posts;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
@@ -9,10 +12,14 @@
     public class PostsController : Controller
     {
         private readonly IPostCategoriesService postCategoriesService;
+        private readonly IPostsService postsService;
 
-        public PostsController(IPostCategoriesService postCategoriesService)
+        public PostsController(
+            IPostCategoriesService postCategoriesService,
+            IPostsService postsService)
         {
             this.postCategoriesService = postCategoriesService;
+            this.postsService = postsService;
         }
 
         public IActionResult Categories()
@@ -20,6 +27,44 @@
             var viewModel = this.postCategoriesService.GetAll<PostCategoryViewModel>();
 
             return this.View(viewModel);
+        }
+
+        public IActionResult Category(int categoryId)
+        {
+            var posts = this.postsService.GetPostsByCategory<PostViewModel>(categoryId);
+
+            var viewModel = new CategoryPostsViewModel
+            {
+                Name = this.postCategoriesService.GetName(categoryId),
+                Posts = posts,
+            };
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PostInputModel model)
+        {
+            if (this.postCategoriesService.IsExist(model.CategoryId) == false)
+            {
+                this.ModelState.AddModelError("Category", "Invalid category!");
+            }
+
+            if (this.ModelState.IsValid == false)
+            {
+                return this.View(model);
+            }
+
+            model.AuthorId = this.User.GetUserId();
+
+            await this.postsService.CreateAsync(model);
+
+            return this.RedirectToAction(nameof(this.Categories));
         }
     }
 }
