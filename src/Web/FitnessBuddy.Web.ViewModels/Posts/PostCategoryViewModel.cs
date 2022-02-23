cf20e266b2b421bcff5acf1f1472404bb264760a
Dimitar Beshev <beshev1@gmail.com>
2022-02-23 16:54:30 +0200

    Add post category view model.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Posts/PostCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Posts
+{
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class PostCategoryViewModel : IMapFrom<PostCategory>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int PostsCount { get; set; }
+    }
+}
