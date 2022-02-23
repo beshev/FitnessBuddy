56494252a6bf523c5c3c557bfc3617cd3ddff6d6
Dimitar Beshev <beshev1@gmail.com>
2022-02-23 16:53:55 +0200

    Add posts controller.

---
+++ src/Web/FitnessBuddy.Web/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿namespace FitnessBuddy.Web.Controllers
+{
+    using FitnessBuddy.Services.Data.Posts;
+    using FitnessBuddy.Web.ViewModels.Posts;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PostsController : Controller
+    {
+        private readonly IPostCategoriesService postCategoriesService;
+
+        public PostsController(IPostCategoriesService postCategoriesService)
+        {
+            this.postCategoriesService = postCategoriesService;
+        }
+
+        public IActionResult Categories()
+        {
+            var viewModel = this.postCategoriesService.GetAll<PostCategoryViewModel>();
+
+            return this.View(viewModel);
+        }
+    }
+}
