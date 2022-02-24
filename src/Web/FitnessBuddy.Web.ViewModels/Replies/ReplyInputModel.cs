788e2b563c5bb3917a74e23e79da3e154e9a3f64
Dimitar Beshev <beshev1@gmail.com>
2022-02-24 15:30:16 +0200

    Add replies view models.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Replies/ReplyInputModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Replies
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using FitnessBuddy.Common;
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class ReplyInputModel : IMapTo<Reply>
+    {
+        [Required]
+        [StringLength(DataConstants.ReplyDescriptionMaxLength, MinimumLength = DataConstants.ReplyDescriptionMinLength)]
+        public string Description { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public int PostId { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web.ViewModels/Replies/ReplyViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Replies
+{
+    using System;
+
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class ReplyViewModel : IMapFrom<Reply>
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string AuthorUsername { get; set; }
+
+        public int PostId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
