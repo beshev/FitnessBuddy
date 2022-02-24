90a1fe44752155889bf46b0fea9e0c0eb964e453
Dimitar Beshev <beshev1@gmail.com>
2022-02-24 15:30:28 +0200

    Add post details view model.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Posts/PostDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Posts
+{
+    using System;
+    using System.Collections.Generic;
+
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+    using FitnessBuddy.Web.ViewModels.Replies;
+
+    public class PostDetailsViewModel : IMapFrom<Post>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string AuthorUsername { get; set; }
+
+        public string Description { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public ICollection<ReplyViewModel> Replies { get; set; }
+    }
+}
