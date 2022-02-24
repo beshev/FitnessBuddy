3c2ea0f9768b6edcdf1b43ca79ca0ac79d843b4b
Dimitar Beshev <beshev1@gmail.com>
2022-02-24 15:28:04 +0200

    Add reply view component.

---
+++ src/Web/FitnessBuddy.Web/ViewComponents/AddReplyViewComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace FitnessBuddy.Web.ViewComponents
+{
+    using FitnessBuddy.Web.ViewModels.Replies;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AddReplyViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int postId, int? parentId = null)
+        {
+            var viewModel = new ReplyInputModel
+            {
+                PostId = postId,
+                ParentId = parentId,
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
