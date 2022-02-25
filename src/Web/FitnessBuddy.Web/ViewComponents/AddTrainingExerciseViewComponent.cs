7875dc2fc148e5c963bddecd12570682b4ed899f
Dimitar Beshev <beshev1@gmail.com>
2022-02-25 18:39:56 +0200

    Add view component for adding exercise to training.

---
+++ src/Web/FitnessBuddy.Web/ViewComponents/AddTrainingExerciseViewComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnessBuddy.Web.ViewComponents
+{
+    using FitnessBuddy.Web.ViewModels.Trainings;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AddTrainingExerciseViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int exerciseId)
+        {
+            var viewModel = new TrainingExerciseInputModel
+            {
+                ExerciseId = exerciseId,
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
