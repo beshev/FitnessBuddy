beb98a26167c16e26ba4e6504e6dd1631b4dd26a
Dimitar Beshev <beshev1@gmail.com>
2022-02-28 13:12:32 +0200

    Scaffold identity Register and Login.

---
+++ src/Web/FitnessBuddy.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using FitnessBuddy.Data;
+using FitnessBuddy.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FitnessBuddy.Web.Areas.Identity.IdentityHostingStartup))]
+
+namespace FitnessBuddy.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
