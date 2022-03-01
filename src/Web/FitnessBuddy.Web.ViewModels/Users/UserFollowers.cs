11b16d1adba39f900bd65535ce62d4d530f22a88
Dimitar Beshev <beshev1@gmail.com>
2022-03-01 14:22:25 +0200

    Add view model for user followers and following.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Users/UserFollowers.cs
@@ -0,0 +1,27 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Users
+{
+    using System.IO;
+
+    using AutoMapper;
+    using FitnessBuddy.Data.Models;
+
+    public class UserFollowers : ShortUserViewModel
+    {
+        public override void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<UserFollower, UserFollowers>()
+                .ForMember(
+                dest => dest.FollowingCount,
+                opt => opt.MapFrom(x => x.Follower.Following.Count))
+                .ForMember(
+                dest => dest.FollowersCount,
+                opt => opt.MapFrom(x => x.Follower.Followers.Count))
+                .ForMember(
+                dest => dest.Username, opt => opt.MapFrom(x => x.Follower.UserName))
+                .ForMember(
+                dest => dest.Email, opt => opt.MapFrom(x => x.Follower.Email))
+                .ForMember(
+                dest => dest.ProfilePicture, opt => opt.MapFrom(x => $"/images/profileimages/{Path.GetFileName(x.Follower.ProfilePicture)}"));
+        }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web.ViewModels/Users/UserFollowing.cs
@@ -0,0 +1,27 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Users
+{
+    using System.IO;
+
+    using AutoMapper;
+    using FitnessBuddy.Data.Models;
+
+    public class UserFollowing : ShortUserViewModel
+    {
+        public override void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<UserFollower, UserFollowing>()
+                .ForMember(
+                dest => dest.FollowingCount,
+                opt => opt.MapFrom(x => x.User.Following.Count))
+                .ForMember(
+                dest => dest.FollowersCount,
+                opt => opt.MapFrom(x => x.User.Followers.Count))
+                .ForMember(
+                dest => dest.Username, opt => opt.MapFrom(x => x.User.UserName))
+                .ForMember(
+                dest => dest.Email, opt => opt.MapFrom(x => x.User.Email))
+                .ForMember(
+                dest => dest.ProfilePicture, opt => opt.MapFrom(x => $"/images/profileimages/{Path.GetFileName(x.User.ProfilePicture)}"));
+        }
+    }
+}
