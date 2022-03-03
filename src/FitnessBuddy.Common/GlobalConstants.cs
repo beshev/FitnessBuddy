e3f75c048d027cf68293b55b1b0ac11447994b4d
Dimitar Beshev <beshev1@gmail.com>
2022-03-03 17:53:09 +0200

    Add some constants.

--- src/FitnessBuddy.Common/GlobalConstants.cs
+++ src/FitnessBuddy.Common/GlobalConstants.cs
@@ -15,9 +15,19 @@
         public const string MyMealsUrl = "/Meals/MyMeals";
         public const string UserTrainings = "/Trainings/MyTrainings";
 
-        // Followers, Following and Tab
+        // Names
         public const string NameOfFollowers = "followers";
         public const string NameOfFollowing = "following";
         public const string NameOfTab = "tab";
+        public const string NameOfDelete = "delete";
+        public const string NameOfSuccess = "success";
+        public const string NameOfExercise = "exercise";
+        public const string NameOfFood = "food";
+
+        // CRUD messages
+        public const string DeleteMessage = "The {0} was deleted successfully";
+        public const string SuccessMessage = "The {0} was added successfully";
+        public const string AddFoodToFavoriteMessage = "The food was added successfully to favorites";
+        public const string RemoveFoodFromFavoriteMessage = "The food was removed successfully from favorites";
     }
 }
