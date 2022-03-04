c611b35215dec698cc5a79fe28cf166828bac574
Dimitar Beshev <beshev1@gmail.com>
2022-03-04 12:12:18 +0200

    Add script.

--- src/Web/FitnessBuddy.Web/wwwroot/js/site.js
+++ src/Web/FitnessBuddy.Web/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+
+    $('.counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
