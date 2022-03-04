db25d93259b7a639ff22822a11f6c04adb9019a9
Dimitar Beshev <beshev1@gmail.com>
2022-03-04 18:10:09 +0200

    Add view model.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Posts/AllPostsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Posts
+{
+    using System.Collections.Generic;
+
+    public class AllPostsViewModel : PagingViewModel
+    {
+        public IEnumerable<PostViewModel> Posts { get; set; }
+    }
+}
