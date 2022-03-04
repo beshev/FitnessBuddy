2049c8607de1cde1968f12fc9af6784de8d367ef
Dimitar Beshev <beshev1@gmail.com>
2022-03-04 19:17:39 +0200

    Add view models.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Users/AdministrationUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Users
+{
+    using System;
+
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Data.Models.Enums;
+    using FitnessBuddy.Services.Mapping;
+
+    public class AdministrationUserViewModel : IMapFrom<ApplicationUser>
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public GenderType Gender { get; set; }
+
+        public bool IsBanned { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web.ViewModels/Users/AllUsersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Users
+{
+    using System.Collections.Generic;
+
+    public class AllUsersViewModel : PagingViewModel
+    {
+        public IEnumerable<AdministrationUserViewModel> Users { get; set; }
+    }
+}
