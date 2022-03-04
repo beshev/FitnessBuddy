2928fa7936dce72c82a69abf7b8da70901e61b02
Dimitar Beshev <beshev1@gmail.com>
2022-03-04 19:16:21 +0200

    Add administrations for processing users.

---
+++ src/Web/FitnessBuddy.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿namespace FitnessBuddy.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Services.Data.Users;
+    using FitnessBuddy.Web.ViewModels.Users;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UsersController : AdministrationController
+    {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public IActionResult All(int id = 1)
+        {
+            if (id < 1)
+            {
+                return this.NotFound();
+            }
+
+            var usersPerPage = 15;
+            int count = this.usersService.GetCount();
+            int pagesCount = (int)Math.Ceiling((double)count / usersPerPage);
+
+            if (id > pagesCount)
+            {
+                return this.NotFound();
+            }
+
+            var skip = (id - 1) * usersPerPage;
+
+            var users = this.usersService.GetAll<AdministrationUserViewModel>(null, skip, usersPerPage);
+
+            var viewModel = new AllUsersViewModel
+            {
+                PageNumber = id,
+                PagesCount = pagesCount,
+                Users = users,
+                ForAction = nameof(this.All),
+                ForController = this.GetType().Name.Replace(nameof(Controller), string.Empty),
+            };
+
+            return this.View(viewModel);
+        }
+
+        public async Task<IActionResult> Ban(string username)
+        {
+            return this.RedirectToAction(nameof(this.All));
+        }
+
+        public async Task<IActionResult> Unban(string username)
+        {
+            return this.RedirectToAction(nameof(this.All));
+        }
+    }
+}
