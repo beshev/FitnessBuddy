cbb465cb91fd46600ad35ced43f3774ccf5d51ff
Dimitar Beshev <beshev1@gmail.com>
2022-03-04 16:02:03 +0200

    Add administrations for processing posts.

---
+++ src/Web/FitnessBuddy.Web/Areas/Administration/Controllers/PostsController.cs
@@ -0,0 +1,126 @@
+﻿namespace FitnessBuddy.Web.Areas.Administration.Controllers
+{
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Services.Data.Posts;
+    using FitnessBuddy.Web.Infrastructure.Extensions;
+    using FitnessBuddy.Web.ViewModels.Posts;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PostsController : AdministrationController
+    {
+        private readonly IPostsService postsService;
+
+        public PostsController(IPostsService postsService)
+        {
+            this.postsService = postsService;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = this.postsService.GetAll<PostViewModel>();
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var post = this.postsService.GetById<PostDetailsViewModel>(id.Value);
+
+            if (post == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(post);
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PostInputModel model)
+        {
+            if (this.ModelState.IsValid == false)
+            {
+                return this.View(model);
+            }
+
+            model.AuthorId = this.User.GetUserId();
+            await this.postsService.CreateAsync(model);
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var post = this.postsService.GetById<PostInputModel>(id.Value);
+
+            if (post == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PostInputModel model)
+        {
+            if (this.postsService.IsExist(model.Id) == false)
+            {
+                return this.NotFound();
+            }
+
+            if (this.ModelState.IsValid == false)
+            {
+                return this.View(model);
+            }
+
+            await this.postsService.EditAsync(model);
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var post = this.postsService.GetById<PostDetailsViewModel>(id.Value);
+
+            if (post == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(post);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await this.postsService.DeleteAsync(id);
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
