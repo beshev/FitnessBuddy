bb240f437319482b4724c49ac9ac5cec3f23131f
Dimitar Beshev <beshev1@gmail.com>
2022-03-04 13:24:21 +0200

    Add display attributes.

--- src/Web/FitnessBuddy.Web.ViewModels/Foods/FoodViewModel.cs
+++ src/Web/FitnessBuddy.Web.ViewModels/Foods/FoodViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace FitnessBuddy.Web.ViewModels.Foods
 {
+    using System.ComponentModel.DataAnnotations;
+
     using AutoMapper;
     using FitnessBuddy.Common;
     using FitnessBuddy.Data.Models;
@@ -9,19 +11,28 @@
     {
         public int Id { get; set; }
 
+        [Display(Name = "Food name")]
         public string FoodName { get; set; }
 
+        [Display(Name = "Protein per 100 grams")]
         public double ProteinIn100Grams { get; set; }
 
+        [Display(Name = "Carbohydrates per 100 grams")]
         public double CarbohydratesIn100Grams { get; set; }
 
+        [Display(Name = "Fat per 100 grams")]
         public double FatIn100Grams { get; set; }
 
+        [Display(Name = "Sodium per 100 grams")]
         public double Sodium { get; set; }
+
+        [Display(Name = "Added by")]
+        public string AddedByUserUsername { get; set; }
 
         public string FoodCalories
             => (((this.ProteinIn100Grams + this.CarbohydratesIn100Grams) * GlobalConstants.CaloriesForOneGramProteinAndCarbohydrates) + (this.FatIn100Grams * GlobalConstants.CaloriesForOneGramFats)).ToString("F2");
 
+        [Display(Name = "Image Url")]
         public string ImageUrl { get; set; }
 
         public void CreateMappings(IProfileExpression configuration)
