4eb263d02e1cc7c422b7d1b3d35e782b1a215141
Dimitar Beshev <beshev1@gmail.com>
2022-03-14 13:52:15 +0200

    Add view component for creating modal for delete confirmation.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/DeleteModalViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessBuddy.Web.ViewModels
+{
+    public class DeleteModalViewModel
+    {
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string ModalId { get; set; }
+
+        public string ContorollerName { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web/ViewComponents/DeleteModalViewComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace FitnessBuddy.Web.ViewComponents
+{
+    using FitnessBuddy.Web.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DeleteModalViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int itemId, string controllerName, string itemName)
+        {
+            var viewModel = new DeleteModalViewModel
+            {
+                ItemId = itemId,
+                ItemName = itemName,
+                ContorollerName = controllerName,
+                ModalId = $"deleteModal{itemId}",
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
