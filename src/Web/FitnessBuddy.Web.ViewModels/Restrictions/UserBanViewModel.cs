b159e4976a5f58f07e42f4c33967210db2c5560e
Dimitar Beshev <beshev1@gmail.com>
2022-03-14 16:08:01 +0200

    Add restriction controller with ban view and user ban model.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Restrictions/UserBanViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Restrictions
+{
+    using System;
+
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class UserBanViewModel : IMapFrom<ApplicationUser>
+    {
+        public string BanReason { get; set; }
+
+        public DateTime BannedOn { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web/Controllers/RestrictionsController.cs
@@ -0,0 +1,33 @@
+﻿namespace FitnessBuddy.Web.Controllers
+{
+    using FitnessBuddy.Services.Data.Users;
+    using FitnessBuddy.Web.Infrastructure.Extensions;
+    using FitnessBuddy.Web.ViewModels.Restrictions;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class RestrictionsController : Controller
+    {
+        private readonly IUsersService usersService;
+
+        public RestrictionsController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public IActionResult Ban()
+        {
+            var userId = this.User.GetUserId();
+
+            if (this.usersService.IsUserBanned(userId) == false)
+            {
+                return this.NotFound();
+            }
+
+            var viewModel = this.usersService.GetUserInfo<UserBanViewModel>(userId);
+
+            return this.View(viewModel);
+        }
+    }
+}
