73181543276337f47ded8432495597e9c0bdf4f7
Dimitar Beshev <beshev1@gmail.com>
2022-03-16 12:05:37 +0200

    Add guest users' home view.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    using FitnessBuddy.Web.ViewModels.Exercises;
+    using FitnessBuddy.Web.ViewModels.Foods;
+
+    public class HomeViewModel
+    {
+        public IEnumerable<ExerciseViewModel> Exercises { get; set; }
+
+        public IEnumerable<FoodViewModel> Foods { get; set; }
+    }
+}
--- src/Web/FitnessBuddy.Web/Controllers/HomeController.cs
+++ src/Web/FitnessBuddy.Web/Controllers/HomeController.cs
@@ -2,16 +2,40 @@
 {
     using System.Diagnostics;
 
+    using FitnessBuddy.Services.Data.Exercises;
+    using FitnessBuddy.Services.Data.Foods;
     using FitnessBuddy.Web.Infrastructure.Filters;
     using FitnessBuddy.Web.ViewModels;
+    using FitnessBuddy.Web.ViewModels.Exercises;
+    using FitnessBuddy.Web.ViewModels.Foods;
+    using FitnessBuddy.Web.ViewModels.Home;
     using Microsoft.AspNetCore.Mvc;
 
     [TypeFilter(typeof(RestrictBannedUsersAttribute))]
     public class HomeController : Controller
     {
+        private readonly IExercisesService exercisesService;
+        private readonly IFoodsService foodsService;
+
+        public HomeController(
+            IExercisesService exercisesService,
+            IFoodsService foodsService)
+        {
+            this.exercisesService = exercisesService;
+            this.foodsService = foodsService;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var count = 2;
+
+            var viewModel = new HomeViewModel
+            {
+                Exercises = this.exercisesService.GetRandom<ExerciseViewModel>(count),
+                Foods = this.foodsService.GetRandom<FoodViewModel>(count),
+            };
+
+            return this.View(viewModel);
         }
 
         public IActionResult Privacy()
