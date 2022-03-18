09cb6dd6000859736344acdfdeeee2f6b8f9208f
Dimitar Beshev <beshev1@gmail.com>
2022-03-18 20:37:16 +0200

    Add exercise likes api controller.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/ExercisesLikes/ExerciseLikeReturnModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessBuddy.Web.ViewModels.ExercisesLikes
+{
+    public class ExerciseLikeReturnModel
+    {
+        public int LikesCount { get; set; }
+
+        public bool IsUserLikeExercise { get; set; }
+    }
+}
---
+++ src/Web/FitnessBuddy.Web/Controllers/ExercisesLikesController.cs
@@ -0,0 +1,50 @@
+﻿namespace FitnessBuddy.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Services.Data.Exercises;
+    using FitnessBuddy.Services.Data.ExercisesLikes;
+    using FitnessBuddy.Web.Infrastructure.Extensions;
+    using FitnessBuddy.Web.ViewModels.ExercisesLikes;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ExercisesLikesController : ApiController
+    {
+        private readonly IExercisesLikesService exercisesLikesService;
+        private readonly IExercisesService exercisesService;
+
+        public ExercisesLikesController(
+            IExercisesLikesService exercisesLikesService,
+            IExercisesService exercisesService)
+        {
+            this.exercisesLikesService = exercisesLikesService;
+            this.exercisesService = exercisesService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ExerciseLikeReturnModel>> Post(int exerciseId)
+        {
+            var userId = this.User.GetUserId();
+            var isLike = this.exercisesLikesService.IsExists(userId, exerciseId);
+
+            if (isLike)
+            {
+                await this.exercisesLikesService.UnLikeAsync(userId, exerciseId);
+                isLike = false;
+            }
+            else if (this.exercisesService.IsExist(exerciseId))
+            {
+                await this.exercisesLikesService.LikeAsync(userId, exerciseId);
+                isLike = true;
+            }
+
+            var returnModel = new ExerciseLikeReturnModel()
+            {
+                LikesCount = this.exercisesLikesService.GetExerciseLikesCount(exerciseId),
+                IsUserLikeExercise = isLike,
+            };
+
+            return returnModel;
+        }
+    }
+}
