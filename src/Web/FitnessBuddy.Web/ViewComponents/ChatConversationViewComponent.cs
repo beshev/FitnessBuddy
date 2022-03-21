15827b69f5aa30f5dd0fca436acaa8ef9b2179fa
Dimitar Beshev <beshev1@gmail.com>
2022-03-21 21:58:26 +0200

    Chat conversation view component.

---
+++ src/Web/FitnessBuddy.Web/ViewComponents/ChatConversationViewComponent.cs
@@ -0,0 +1,26 @@
+﻿namespace FitnessBuddy.Web.ViewComponents
+{
+    using FitnessBuddy.Services.Data.Messages;
+    using FitnessBuddy.Web.Infrastructure.Extensions;
+    using FitnessBuddy.Web.ViewModels.Chats;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ChatConversationViewComponent : ViewComponent
+    {
+        private readonly IMessagesService messagesService;
+
+        public ChatConversationViewComponent(IMessagesService messagesService)
+        {
+            this.messagesService = messagesService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var userId = this.UserClaimsPrincipal.GetUserId();
+
+            var viewModel = this.messagesService.GetConversations<UserChatViewModel>(userId);
+
+            return this.View(viewModel);
+        }
+    }
+}
