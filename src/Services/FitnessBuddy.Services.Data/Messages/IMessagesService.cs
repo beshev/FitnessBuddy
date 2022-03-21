f2e6cf834eaf46352a27ae56c85b79746764776a
Dimitar Beshev <beshev1@gmail.com>
2022-03-21 11:03:48 +0200

    Add messages service.

---
+++ src/Services/FitnessBuddy.Services.Data/Messages/IMessagesService.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessBuddy.Services.Data.Messages
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IMessagesService
+    {
+        public Task SendMessageAsync(string authorId, string receiverId, string content);
+
+        public IEnumerable<TModel> GetMessages<TModel>(string firstUserId, string secondUserId);
+
+        public Task DeleteMessageAsync(string authorId, string receiverId);
+    }
+}
---
+++ src/Services/FitnessBuddy.Services.Data/Messages/MessagesService.cs
@@ -0,0 +1,54 @@
+﻿namespace FitnessBuddy.Services.Data.Messages
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FitnessBuddy.Data.Common.Repositories;
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class MessagesService : IMessagesService
+    {
+        private readonly IDeletableEntityRepository<Message> messagesRepository;
+
+        public MessagesService(IDeletableEntityRepository<Message> messagesRepository)
+        {
+            this.messagesRepository = messagesRepository;
+        }
+
+        public async Task DeleteMessageAsync(string authorId, string receiverId)
+        {
+            var message = this.messagesRepository
+                .All()
+                .FirstOrDefault(x => x.AuthorId == authorId && x.ReceiverId == receiverId);
+
+            this.messagesRepository.Delete(message);
+
+            await this.messagesRepository.SaveChangesAsync();
+        }
+
+        public IEnumerable<TModel> GetMessages<TModel>(string firstUserId, string secondUserId)
+            => this.messagesRepository
+            .AllAsNoTrackingWithDeleted()
+            .Where(x =>
+                (x.AuthorId == firstUserId && x.ReceiverId == secondUserId)
+                || (x.AuthorId == secondUserId && x.ReceiverId == firstUserId))
+            .OrderBy(x => x.CreatedOn)
+            .To<TModel>()
+            .AsEnumerable();
+
+        public async Task SendMessageAsync(string authorId, string receiverId, string content)
+        {
+            var message = new Message
+            {
+                AuthorId = authorId,
+                ReceiverId = receiverId,
+                Content = content,
+            };
+
+            await this.messagesRepository.AddAsync(message);
+            await this.messagesRepository.SaveChangesAsync();
+        }
+    }
+}
