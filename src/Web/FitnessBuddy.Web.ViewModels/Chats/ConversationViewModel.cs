df2cfbaca98a0da13d7dfbe245f50de343d6951d
Dimitar Beshev <beshev1@gmail.com>
2022-03-22 16:26:56 +0200

    Add model for conversations.

---
+++ src/Web/FitnessBuddy.Web.ViewModels/Chats/ConversationViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FitnessBuddy.Web.ViewModels.Chats
+{
+    using FitnessBuddy.Data.Models;
+    using FitnessBuddy.Services.Mapping;
+
+    public class ConversationViewModel : IMapFrom<ApplicationUser>
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string LastMessage { get; set; }
+
+        public string LastActivity { get; set; }
+    }
+}
