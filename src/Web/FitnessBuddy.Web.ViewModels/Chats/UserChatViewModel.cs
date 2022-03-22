bd708cd69022b85c37fa9bfe3e99b5b653175837
Dimitar Beshev <beshev1@gmail.com>
2022-03-22 16:26:25 +0200

    Remove propery.

--- src/Web/FitnessBuddy.Web.ViewModels/Chats/UserChatViewModel.cs
+++ src/Web/FitnessBuddy.Web.ViewModels/Chats/UserChatViewModel.cs
@@ -1,6 +1,5 @@
 ﻿namespace FitnessBuddy.Web.ViewModels.Chats
 {
-    using System;
     using System.Collections.Generic;
 
     using AutoMapper;
@@ -13,8 +12,6 @@
 
         public string ReceiverUsername { get; set; }
 
-        public string CreatedOn { get; set; }
-
         public IEnumerable<MessageViewModel> Messages { get; set; }
 
         public void CreateMappings(IProfileExpression configuration)
@@ -23,9 +20,7 @@
                 .ForMember(
                 dest => dest.ReceiverId, opt => opt.MapFrom(x => x.Id))
                 .ForMember(
-                dest => dest.ReceiverUsername, opt => opt.MapFrom(x => x.UserName))
-                .ForMember(
-                dest => dest.CreatedOn, opt => opt.MapFrom(x => x.CreatedOn.ToString("MM/dd/yyyy HH:mm")));
+                dest => dest.ReceiverUsername, opt => opt.MapFrom(x => x.UserName));
         }
     }
 }
