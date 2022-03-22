ba101179cda6fabcf5a8a49d68e0a9c13128f61f
Dimitar Beshev <beshev1@gmail.com>
2022-03-22 14:23:10 +0200

    Service for generating a group name.

---
+++ src/Services/FitnessBuddy.Services/Hubs/GroupNameProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace FitnessBuddy.Services.Hubs
+{
+    public class GroupNameProvider : IGroupNameProvider
+    {
+        public string GetGroupName(string firstSrting, string secondString)
+            => firstSrting.CompareTo(secondString) > 0
+                ? $"{secondString}{firstSrting}"
+                : $"{firstSrting}{secondString}";
+    }
+}
---
+++ src/Services/FitnessBuddy.Services/Hubs/IGroupNameProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace FitnessBuddy.Services.Hubs
+{
+    public interface IGroupNameProvider
+    {
+        public string GetGroupName(string firstSrting, string secondString);
+    }
+}
