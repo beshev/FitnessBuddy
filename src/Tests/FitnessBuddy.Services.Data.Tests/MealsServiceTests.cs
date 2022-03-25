bd2a651f8495bbcafa948b6ba7d3b97db304ceb8
Dimitar Beshev <beshev1@gmail.com>
2022-03-25 10:46:44 +0200

    Add Fluent Assertions library and change assertion methods.

--- src/Tests/FitnessBuddy.Services.Data.Tests/MealsServiceTests.cs
+++ src/Tests/FitnessBuddy.Services.Data.Tests/MealsServiceTests.cs
@@ -6,12 +6,15 @@
     using System.Reflection;
     using System.Threading.Tasks;
 
+    using AutoMapper;
+    using FitnessBuddy.Data.Common.Models;
     using FitnessBuddy.Data.Common.Repositories;
     using FitnessBuddy.Data.Models;
     using FitnessBuddy.Services.Data.Meals;
     using FitnessBuddy.Services.Mapping;
     using FitnessBuddy.Web.ViewModels;
     using FitnessBuddy.Web.ViewModels.Meals;
+    using FluentAssertions;
     using MockQueryable.Moq;
     using Moq;
     using Xunit;
@@ -21,11 +24,11 @@
         [Fact]
         public async Task CreateMethodShouldCreateMeal()
         {
-            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+            this.InitializeAutoMapper<MealInputModel, Meal>();
 
             var list = new List<Meal>();
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.AddAsync(It.IsAny<Meal>())).Callback((Meal meal) => list.Add(meal));
 
             var service = new MealsService(mockRepo.Object);
@@ -42,12 +45,9 @@
 
             var addedMeal = await service.CreateAsync(userId, meal);
 
-            Assert.Single(list);
-            Assert.Equal(meal.Name, addedMeal.Name);
-            Assert.Equal(meal.TargetCarbs, addedMeal.TargetCarbs);
-            Assert.Equal(meal.TargetFat, addedMeal.TargetFat);
-            Assert.Equal(meal.TargetProtein, addedMeal.TargetProtein);
-            Assert.Equal(userId, addedMeal.ForUserId);
+            list.Should().HaveCount(1);
+            addedMeal.Should().BeEquivalentTo(meal);
+            addedMeal.ForUserId.Should().Be(userId);
         }
 
         [Fact]
@@ -64,7 +64,7 @@
 
             var dateTime = DateTime.UtcNow;
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
             mockRepo.Setup(x => x.Delete(It.IsAny<Meal>())).Callback((Meal meal) =>
             {
@@ -76,8 +76,8 @@
 
             var meal = await service.DeleteAsync(1);
 
-            Assert.True(meal.IsDeleted);
-            Assert.Equal(dateTime, meal.DeletedOn);
+            meal.IsDeleted.Should().BeTrue();
+            meal.DeletedOn.Should().Be(dateTime);
         }
 
         [Fact]
@@ -88,12 +88,12 @@
                 new Meal { Id = 2, Name = "Test" },
             };
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.AllAsNoTracking()).Returns(list.AsQueryable().BuildMock());
 
             var service = new MealsService(mockRepo.Object);
 
-            Assert.False(await service.IsExistAsync(1));
+            (await service.IsExistAsync(1)).Should().BeFalse();
         }
 
         [Fact]
@@ -104,7 +104,7 @@
                 new Meal { Id = 2, Name = "Test" },
             };
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.AllAsNoTracking()).Returns(list.AsQueryable().BuildMock());
 
             var service = new MealsService(mockRepo.Object);
@@ -121,15 +121,15 @@
                 new Meal { Name = "Test 2", Id = 2 },
             };
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.AllAsNoTracking()).Returns(list.AsQueryable().BuildMock());
 
             var service = new MealsService(mockRepo.Object);
 
-            var actualMeal = await service.GetByIdAsync(2);
+            var actual = await service.GetByIdAsync(2);
 
-            Assert.Equal("Test 2", actualMeal.Name);
-            Assert.Equal(2, actualMeal.Id);
+            actual.Name.Should().Be("Test 2");
+            actual.Id.Should().Be(2);
         }
 
         [Fact]
@@ -141,20 +141,20 @@
                 new Meal { Name = "Test 2", Id = 2 },
             };
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.AllAsNoTracking()).Returns(list.AsQueryable().BuildMock());
 
             var service = new MealsService(mockRepo.Object);
 
             var actualMeal = await service.GetByIdAsync(3);
 
-            Assert.Null(actualMeal);
+            actualMeal.Should().BeNull();
         }
 
         [Fact]
         public async Task GetUserMealsMethodShouldReturnEmptyCollectionIfUserDontHaveMeals()
         {
-            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+            this.InitializeAutoMapper<Meal>();
 
             var list = new List<Meal>
             {
@@ -162,22 +162,22 @@
                  new Meal { Name = "Test", Id = 2, ForUserId = "1" },
             };
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.AllAsNoTracking()).Returns(list.AsQueryable().BuildMock());
 
             var service = new MealsService(mockRepo.Object);
 
             var userId = "2";
 
-            var actualList = await service.GetUserMealsAsync<MealViewModel>(userId);
+            var actualList = await service.GetUserMealsAsync<Meal>(userId);
 
-            Assert.Empty(actualList);
+            actualList.Should().BeEmpty();
         }
 
         [Fact]
         public async Task GetUserMealsMethodShouldReturnUserMeals()
         {
-            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+            this.InitializeAutoMapper<Meal>();
 
             var list = new List<Meal>
             {
@@ -185,18 +185,17 @@
                  new Meal { Name = "Test 2", Id = 2, ForUserId = "1" },
             };
 
-            var mockRepo = new Mock<IDeletableEntityRepository<Meal>>();
+            var mockRepo = this.MockDeletableRepository<Meal>();
             mockRepo.Setup(x => x.AllAsNoTracking()).Returns(list.AsQueryable().BuildMock());
 
             var service = new MealsService(mockRepo.Object);
 
             var userId = "1";
 
-            var actualList = (await service.GetUserMealsAsync<MealViewModel>(userId)).ToList();
+            var actualList = (await service.GetUserMealsAsync<Meal>(userId)).ToList();
 
-            Assert.Equal(2, actualList.Count);
-            Assert.Equal("Test", actualList[0].Name);
-            Assert.Equal("Test 2", actualList[1].Name);
+            actualList.Should().HaveCount(2);
+            actualList.Should().BeEquivalentTo(list);
         }
 
         [Fact]
@@ -215,8 +214,8 @@
 
             var userId = "2";
 
-            Assert.False(await service.IsUserMealAsync(1, userId));
-            Assert.False(await service.IsUserMealAsync(2, userId));
+            (await service.IsUserMealAsync(1, userId)).Should().BeFalse();
+            (await service.IsUserMealAsync(21, userId)).Should().BeFalse();
         }
 
         [Fact]
@@ -235,8 +234,33 @@
 
             var userId = "1";
 
-            Assert.True(await service.IsUserMealAsync(1, userId));
-            Assert.True(await service.IsUserMealAsync(2, userId));
+            (await service.IsUserMealAsync(1, userId)).Should().BeTrue();
+            (await service.IsUserMealAsync(2, userId)).Should().BeTrue();
+        }
+
+        private void InitializeAutoMapper<TSource>()
+            where TSource : class
+        {
+            var config = new MapperConfigurationExpression();
+            config.CreateMap<TSource, TSource>();
+
+            AutoMapperConfig.MapperInstance = new Mapper(new MapperConfiguration(config));
+        }
+
+        private void InitializeAutoMapper<TSource, TDestination>()
+            where TSource : class
+            where TDestination : class
+        {
+            var config = new MapperConfigurationExpression();
+            config.CreateMap<TSource, TDestination>();
+
+            AutoMapperConfig.MapperInstance = new Mapper(new MapperConfiguration(config));
+        }
+
+        private Mock<IDeletableEntityRepository<TEntity>> MockDeletableRepository<TEntity>()
+           where TEntity : class, IDeletableEntity
+        {
+            return new Mock<IDeletableEntityRepository<TEntity>>();
         }
     }
 }
