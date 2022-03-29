1ea004c4a8259e91d30111462a9487ccf60e6a8b
Dimitar Beshev <beshev1@gmail.com>
2022-03-29 18:11:23 +0300

    Add cloudinary service

---
+++ src/Services/FitnessBuddy.Services/Cloudinary/CloudinaryService.cs
@@ -0,0 +1,53 @@
+﻿namespace FitnessBuddy.Services.Cloudinary
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using CloudinaryDotNet;
+    using CloudinaryDotNet.Actions;
+    using Microsoft.AspNetCore.Http;
+
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary cloudinary;
+
+        public CloudinaryService(Cloudinary cloudinary)
+        {
+            this.cloudinary = cloudinary;
+        }
+
+        public async Task<string> UploadAsync(IFormFile file, string cloudFolder)
+        {
+            if (file == null)
+            {
+                return string.Empty;
+            }
+
+            byte[] destination;
+
+            using (var ms = new MemoryStream())
+            {
+                await file.CopyToAsync(ms);
+                destination = ms.ToArray();
+            }
+
+            UploadResult uploadResult;
+
+            await using (var ms = new MemoryStream(destination))
+            {
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, ms),
+                    PublicId = $"fitnessbuddy/{cloudFolder}",
+                    Overwrite = true,
+                };
+
+                uploadResult = await this.cloudinary.UploadAsync(uploadParams);
+            }
+
+            var secureUrl = uploadResult.Url;
+
+            return secureUrl.OriginalString;
+        }
+    }
+}
---
+++ src/Services/FitnessBuddy.Services/Cloudinary/ICloudinaryService.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessBuddy.Services.Cloudinary
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+
+    public interface ICloudinaryService
+    {
+        public Task<string> UploadAsync(IFormFile file, string fileName);
+    }
+}
